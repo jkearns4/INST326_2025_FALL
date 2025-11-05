7638141a175c3156e488d115d6de8abf34f45a25
dkelil <dkelil@terpmail.umd.edu>
2025-11-04 20:34:10 -0500

    Update INST326_Exercise.py

--- INST326_Exercise.py
+++ INST326_Exercise.py
@@ -6,9 +6,6 @@ print("My name is Josh")
 
 print("Edit by dkelii - My contribution")
 
-<<<<<<< HEAD
 print("My name is Kenny")
 
-=======
 print("Edit by Jharri37 - My contribution")
->>>>>>> dfafceac21aebcdfbfa7f83392ae62484455fcae

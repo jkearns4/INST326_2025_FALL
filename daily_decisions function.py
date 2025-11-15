98e476070e6ff3fc19d441184ccc6ef1781e7241
Jharri37 <jharri37@terpmail.umd.edu>
2025-11-14 22:20:40 -0500

    Update daily_decisions function.py

--- daily_decisions function.py
+++ daily_decisions function.py
@@ -2,6 +2,18 @@ def player_daily_decisions(player_stats, decision_outcome):
 
     """
     This function provides the user with choices each day in the game, takes the input of their decision, and processes it, applying changes to the player stats and printing the results.
+
+    Parameters:
+        player_stats (dict): This is a dictionary that contains the stats of the player.
+        decision_outcome (function): This is a function that takes the player decision as the input and returns a dictionary with keys:
+                                    "result" (str): What the outcome is.
+                                    "severity" (int): What the severity is as a number.
+                                    "stat_changes" (dict): A dictionary of the changes to the player stats.
+
+        returns:
+            tuple:
+                updated_stats (dict): The player's updated stats after the affects of their decisions have taken place.
+                outcome (dict): The outcome of their decision; includes stat_changes.
     """
 
     accepted_decisions = ["hunt", "search_food", "rest", "build_shelter"]

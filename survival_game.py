d5d81d11223bda4226e9f62bd0648868f5c35e28
jkearns4 <jkearns4@terpmail.umd.edu>
2025-12-12 17:59:33 -0500

    fix game class

--- survival_game.py
+++ survival_game.py
@@ -211,41 +211,7 @@ def trigger_random_event(events, player):
 
    return None
 
-class Game:
-   """
-   Primary author: Jason Kearns
 
-
-   Manages the overall game state and main game loop.
-   """
-
-
-   def __init__(self, config):
-       """
-       Primary author: Jason Kearns
-       Techniques claimed: composition of classes (Game has a Player)
-       Initialize the game using configuration data.
-
-       Parameters:
-           config (dict): Configuration dictionary loaded from JSON. It should
-                          contain keys "starting_stats", "daily_decay",
-                          "action_outcomes", "events", and "max_days".
-       """
-       stats = config["starting_stats"]
-       self.player = Player(
-           stats["health"],
-           stats["energy"],
-           stats["hunger"],
-           stats["thirst"],
-           stats["shelter"]
-       )
-
-
-       self.decay = config["daily_decay"]
-       self.actions = config["action_outcomes"]
-       self.events = config["events"]
-       self.max_days = config["max_days"]
-       self.day = 1
 
 
    def choose_action(self):

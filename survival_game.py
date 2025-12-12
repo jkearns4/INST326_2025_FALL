443b0f71dd41108d73855f8d230a2c306111c219
KennethWKong <kennethwkong1@gmail.com>
2025-12-12 00:43:12 -0500

    Update survival_game.py

--- survival_game.py
+++ survival_game.py
@@ -115,3 +115,15 @@ class Game:
 
        print("\nYou survived all days. You win!")
 
+
+# Main entry point
+
+if __name__ == "__main__":
+   # Primary author: Kenneth Kong
+   config = load_config("survival_config.json")
+   game = Game(config)
+   game.run()
+
+
+
+
